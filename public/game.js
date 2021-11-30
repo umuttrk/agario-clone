7a90a4dbdbbd4deed61f5de9274f8b035b5b946a
Umut Türk <74135564+umttrk19@users.noreply.github.com>
2021-12-01 01:11:54 +0300

    scalable

--- public/game.js
+++ public/game.js
@@ -1,67 +1,74 @@
 var socket = io.connect();
 
-$(document).ready(()=> {
-        var circle=new Circle();
-        console.log(circle);
-        socket.emit('add-me-as-player',circle)
-   
-     }
+$(document).ready(() => {
+    var circle = new Circle();
+    console.log(circle);
+    socket.emit('add-me-as-player', circle)
+
+}
 );
-socket.on('place-all-foods',data=>{
+socket.on('place-all-foods', data => {
     data.forEach(element => {
-        $('#parent').append(' <div class="food" id=' + element.id + ' style="   transform: translate(' + element.x + 'px, ' + element.y + 'px); background-color: rgb(' + element.r + ', ' + element.g + ', ' + element.b + ');"></div>');
+        $('#parent').append(' <div class="food" id=' + element.id + ' style=" transform: translate(' + element.x + 'px, ' + element.y + 'px); background-color: rgb(' + element.r + ', ' + element.g + ', ' + element.b + ');"></div>');
     });
 })
-socket.on('place-all-clients-to-my-screen',data=>{
+socket.on('remove-a-food',data=>{
+    
+    console.log(data.element.id+" gone!");
+    var food = document.getElementById(data.element.id);
+    $('#'+data.circle.id).css({"width":data.circle.size+1,"height":data.circle.size+1})
+    food.remove();
+})
+socket.on('place-all-clients-to-my-screen', data => {
     data.forEach(element => {
-        $('#parent').append(' <div class="character" id=' + element.id + ' style="   transform: translate(' + element.x + 'px, ' + element.y + 'px); background-color: rgb(' + element.r + ', ' + element.g + ', ' + element.b + ');"></div>');
+        $('#parent').append(' <div class="character" id=' + element.id + ' style="  width:' + element.size + 'px; height:' + element.size + 'px; transform: translate(' + element.x + 'px, ' + element.y + 'px); background-color: rgb(' + element.r + ', ' + element.g + ', ' + element.b + ');"></div>');
     });
 })
 
-socket.on('place-me-at-all-clients-screen',data=>{
-    $('#parent').append(' <div class="character" id=' + data.id + ' style="   transform: translate(' + data.x + 'px, ' + data.y + 'px); background-color: rgb(' + data.r + ', ' + data.g + ', ' + data.b + ');"></div>');
+socket.on('place-me-at-all-clients-screen', data => {
+    $('#parent').append(' <div class="character" id=' + data.id + ' style="  width:' + element.size + 'px; height:' + element.size + 'px;  transform: translate(' + data.x + 'px, ' + data.y + 'px); background-color: rgb(' + data.r + ', ' + data.g + ', ' + data.b + ');"></div>');
 })
-socket.on('remove-me-from-all-clients-screen',id=>{ 
-    console.log(id);
-   var circle = document.getElementById(id);
-   circle.remove(); 
+socket.on('remove-me-from-all-clients-screen', id => {
+    var circle = document.getElementById(id);
+    circle.remove();
 })
 
-socket.on('move-at-all',(data)=>{
-    $('#'+data.id).css({ "transform": "translate(" + data.x + "px," + data.y + "px)" });
+socket.on('move-at-all', (data) => {
+    $('#' + data.id).css({ "transform": "translate(" + data.x + "px," + data.y + "px)" });
 })
- 
+
 
 document.onkeydown = check_key;
 //basılı tutma, iki tuşa aynı anda basma gibi özellikler
 function check_key(e) {
     e = e || window.event;
-    let command=""
+    let command = ""
     switch (e.keyCode) {
         case 38:
-            command="up";
+            command = "up";
             break;
         case 40:
-            command="down";
+            command = "down";
             break;
         case 37:
-            command="left";
+            command = "left";
             break;
         case 39:
-            command="right";
-           break;
+            command = "right";
+            break;
     }
-    socket.emit('move-command-triggered',command);
-}   
+    socket.emit('move-command-triggered', command);
+}
 
 class Circle {
     constructor(socketId) {
-        this.id=Math.floor(Math.random()*99999);
-        this.socketId=socketId;
-        this.x =  Math.floor(Math.random() * 950);
-        this.y =  Math.floor(Math.random() * 550);
-        this.r =  Math.floor(Math.random() * 255);
-        this.g =  Math.floor(Math.random() * 255);
-        this.b =  Math.floor(Math.random() * 255);
+        this.id = Math.floor(Math.random() * 99999);
+        this.socketId = socketId;
+        this.size = 40;
+        this.x = Math.floor(Math.random() * 950);
+        this.y = Math.floor(Math.random() * 550);
+        this.r = Math.floor(Math.random() * 255);
+        this.g = Math.floor(Math.random() * 255);
+        this.b = Math.floor(Math.random() * 255);
     }
 }
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var onlineCircles = [];
+
+
+
 var foods = [];
 const express = require('express')
 const app = express();
@@ -7,11 +10,14 @@ const server = app.listen(3000, "0.0.0.0", () => {
 })
 const Food = require('./Food');
 const io = require('socket.io')(server);
-for (let index = 0; index < 50; index++) {
+for (let index = 0; index < 200; index++) {
     foods.push(new Food());
 }
 
-const getCircle = (socketId) => onlineCircles.find(e => e.socketId === socketId);
+const getCircle = (socketId) => onlineCircles.find((e, i) => {
+    i += 1;
+    return e.socketId === socketId
+});
 
 io.sockets.on("connection", socket => {
 
@@ -21,27 +27,40 @@ io.sockets.on("connection", socket => {
         onlineCircles.push(data);
         socket.broadcast.emit('place-me-at-all-clients-screen', data);
         socket.emit('place-all-clients-to-my-screen', onlineCircles);
-        io.sockets.emit('place-all-foods',foods);
+        io.sockets.emit('place-all-foods', foods);
     })
 
 
     socket.on('move-command-triggered', command => {
         const circle = getCircle(socket.id);
+        foods.forEach((element, index) => {
+            if (element.x < circle.x + circle.size && element.x > circle.x && element.y < circle.y + circle.size && element.y > circle.y) {
+                console.log(element.id + " yenildi");
+                var a = onlineCircles.findIndex(e => e === circle);
+                onlineCircles[a].size += 1;
+                io.sockets.emit('remove-a-food', { element, circle });
+                foods.splice(index, 1);
+                return null;
+            }
+        });
+
         switch (command) {
             case 'up':
                 circle.y > 0 ? circle.y = circle.y - 20 : circle.y = 0;
                 break;
             case 'down':
-                circle.y < 720 ? circle.y = circle.y + 20 : circle.y = 720;
+                circle.y < 800 - circle.size ? circle.y = circle.y + 20 : circle.y = 800 - circle.size;
                 break;
             case 'left':
                 circle.x > 0 ? circle.x = circle.x - 20 : circle.x = 0;
                 break;
             case 'right':
-                circle.x < 1420 ? circle.x = circle.x + 20 : circle.x = 1420;
+                circle.x < 1500 - circle.size ? circle.x = circle.x + 20 : circle.x = 1500 - circle.size;
                 break;
         }
         io.sockets.emit('move-at-all', circle);
+        // io.sockets.emit('remove-a-food', eatenFood);
+
     })
     socket.on('disconnect', () => {
         console.log("a client disconnected");
